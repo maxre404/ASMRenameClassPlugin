13b5ab896b4b6fce94db83ac1d5e7ca749ccc9cd
winter <max13147>
2023-02-27 15:38:40 +0400

    test

--- app/src/main/java/com/tg/asmrenameclassplugin/MainActivity.kt
+++ app/src/main/java/com/tg/asmrenameclassplugin/MainActivity.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import com.drake.net.utils.scopeNetLife
 
 class MainActivity : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
@@ -12,6 +13,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         var tvTitle = findViewById<TextView>(R.id.tvTitle)
         tvTitle.text = Users().getRealName()
-
+        scopeNetLife {
+            println("bbbbbbbbbbbbb")
+        }
     }
 }
