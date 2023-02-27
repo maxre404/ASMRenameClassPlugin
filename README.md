f9b6109fed4a610b1264d159cb36799122d3cb2e
winter <max13147>
2023-02-27 14:10:09 +0400

    markdown 提交

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ASMRenameClassPlugin
-打包的时候修改类名
+本插件 集成到项目中 会自动寻找当前目录下以下划线结尾的类,重名名并替换原类
+![pic](/img/codebg.png)
+
+
---
+++ app/src/xiaomi/java/com/tg/asmrenameclassplugin/MainActivity_.kt
@@ -0,0 +1,14 @@
+package com.tg.asmrenameclassplugin
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity_ : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.test2)
+    }
+}
